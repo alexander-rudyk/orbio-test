0e699ef315147fc1f06ce4d71ead788c3b4d8a68
alexander-rudyk <alakai.1998.28@gmail.com>
2025-05-15 21:58:36 +0300

    chore: add validation

--- src/files/dtos/files.upload.dto.ts
+++ src/files/dtos/files.upload.dto.ts
@@ -1,3 +1,8 @@
+import { ArrayMinSize, IsArray, IsString } from 'class-validator';
+
 export class FilesUploadDTO {
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1)
   urls: string[];
 }
